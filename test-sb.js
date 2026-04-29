800176450ba7478d0ff1b314aee241f4fe47ce74
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2026-04-30 02:02:29 +0300

    update

--- test-sb.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({ path: '.env.lol' });
-cons{ createClient } = require('@supabase/supabase-js');
-const supabase cat = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-supabase.storage.from('studio-assets').list('frames').then(res => console.log(JSON.stringify(res.data, null, 2)));
